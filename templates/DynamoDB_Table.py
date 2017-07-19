e35db74a0b75e085a6771dad73cb14b8de1f72e6
Mihail Ivanov <mihail.e.ivanov@gmail.com>
2017-07-19 17:49:00 +0100

    Modifying troposphere to be more spectre friendly

--- templates/DynamoDB_Table.py
+++ templates/DynamoDB_Table.py
@@ -7,78 +7,74 @@ from troposphere.dynamodb import (KeySchema, AttributeDefinition,
 from troposphere.dynamodb import Table
 
 class DynamodbTable():
-    t = Template()
+    def __init__(self, sceptre_user_data):
+        self.t = Template()
 
-    t.add_description("AWS CloudFormation Sample Template: This template "
-                      "demonstrates the creation of a DynamoDB table.")
+        self.t.add_description("AWS CloudFormation Sample Template: This template "
+                          "demonstrates the creation of a DynamoDB table.")
 
-    hashkeyname = t.add_parameter(Parameter(
-        "HaskKeyElementName",
-        Description="HashType PrimaryKey Name",
-        Type="String",
-        AllowedPattern="[a-zA-Z0-9]*",
-        MinLength="1",
-        MaxLength="2048",
-        ConstraintDescription="must contain only alphanumberic characters"
-    ))
+        hashkeyname = self.t.add_parameter(Parameter(
+            "HaskKeyElementName",
+            Description="HashType PrimaryKey Name",
+            Type="String",
+            AllowedPattern="[a-zA-Z0-9]*",
+            MinLength="1",
+            MaxLength="2048",
+            ConstraintDescription="must contain only alphanumberic characters"
+        ))
 
-    hashkeytype = t.add_parameter(Parameter(
-        "HaskKeyElementType",
-        Description="HashType PrimaryKey Type",
-        Type="String",
-        Default="S",
-        AllowedPattern="[S|N]",
-        MinLength="1",
-        MaxLength="1",
-        ConstraintDescription="must be either S or N"
-    ))
+        hashkeytype = self.t.add_parameter(Parameter(
+            "HaskKeyElementType",
+            Description="HashType PrimaryKey Type",
+            Type="String",
+            Default="S",
+            AllowedPattern="[S|N]",
+            MinLength="1",
+            MaxLength="1",
+            ConstraintDescription="must be either S or N"
+        ))
 
-    readunits = t.add_parameter(Parameter(
-        "ReadCapacityUnits",
-        Description="Provisioned read throughput",
-        Type="Number",
-        Default="5",
-        MinValue="5",
-        MaxValue="10000",
-        ConstraintDescription="should be between 5 and 10000"
-    ))
+        readunits = self.t.add_parameter(Parameter(
+            "ReadCapacityUnits",
+            Description="Provisioned read throughput",
+            Type="Number",
+            Default="5",
+            MinValue="5",
+            MaxValue="10000",
+            ConstraintDescription="should be between 5 and 10000"
+        ))
 
-    writeunits = t.add_parameter(Parameter(
-        "WriteCapacityUnits",
-        Description="Provisioned write throughput",
-        Type="Number",
-        Default="10",
-        MinValue="5",
-        MaxValue="10000",
-        ConstraintDescription="should be between 5 and 10000"
-    ))
+        writeunits = self.t.add_parameter(Parameter(
+            "WriteCapacityUnits",
+            Description="Provisioned write throughput",
+            Type="Number",
+            Default="10",
+            MinValue="5",
+            MaxValue="10000",
+            ConstraintDescription="should be between 5 and 10000"
+        ))
 
-    myDynamoDB = t.add_resource(Table(
-        "myDynamoDBTable",
-        AttributeDefinitions=[
-            AttributeDefinition(
-                AttributeName=Ref(hashkeyname),
-                AttributeType=Ref(hashkeytype)
-            ),
-        ],
-        KeySchema=[
-            KeySchema(
-                AttributeName=Ref(hashkeyname),
-                KeyType="HASH"
+    def add_dynamoDB_table(self):
+        self.dynamodbtable = self.t.add_resource(Table(
+            "myDynamoDBTable",
+            AttributeDefinitions=[
+                AttributeDefinition(
+                    AttributeName=Ref(hashkeyname),
+                    AttributeType=Ref(hashkeytype)
+                ),
+            ],
+            KeySchema=[
+                KeySchema(
+                    AttributeName=Ref(hashkeyname),
+                    KeyType="HASH"
+                )
+            ],
+            ProvisionedThroughput=ProvisionedThroughput(
+                ReadCapacityUnits=Ref(readunits),
+                WriteCapacityUnits=Ref(writeunits)
             )
-        ],
-        ProvisionedThroughput=ProvisionedThroughput(
-            ReadCapacityUnits=Ref(readunits),
-            WriteCapacityUnits=Ref(writeunits)
-        )
-    ))
-
-    t.add_output(Output(
-        "TableName",
-        Value=Ref(myDynamoDB),
-        Description="Table name of the newly create DynamoDB table",
-    ))
+        ))
 
 def sceptre_handler(sceptre_user_data):
-    dynamodbtalbe = DynamodbTable(sceptre_user_data)
-    return dynamodbtable.template.to_json()
+    dynamodbtable = DynamodbTable(sceptre_user_data)
+    return dynamodbtable.t.to_json()
